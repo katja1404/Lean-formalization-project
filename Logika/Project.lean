7e02c347d612bb43d76156ff1b68f852fddef219
katja1404 <72021351+katja1404@users.noreply.github.com>
2024-06-12 17:50:43 +0200

    Final changes

--- Logika/Project.lean
+++ Logika/Project.lean
@@ -3,11 +3,13 @@ import Mathlib.Data.Nat.Choose.Basic
 import Mathlib.Tactic
 
 
-
+--------- ########### ---------
 --------- Small Tasks ---------
+--------- ########### ---------
 
-
+-- ################## --
 -- 1. Catalan Numbers --
+-- ################## --
 
 open BigOperators
 
@@ -15,10 +17,10 @@ def catalan_number: (n : ℕ) → ℕ
 | 0 => 1
 | n+1 => ∑ i : Fin (n+1), (catalan_number i) * (catalan_number (n-i))
 
--- #eval catalan_number 5
 
-
+-- ############## --
 -- 2. Plane Trees --
+-- ############## --
 
 section plane_tree
 
@@ -30,11 +32,9 @@ inductive plane_tree: Type
 end plane_tree
 
 
--- def plane_tree.definicija : plane_tree -> List plane_tree
--- | .node N => N
-
-
+-- #################### --
 -- 3. Full Binary Trees --
+-- #################### --
 
 inductive full_binary_tree: Type
 | leaf : full_binary_tree
@@ -49,14 +49,18 @@ def full_binary_tree.number_of_nodes : full_binary_tree → ℕ
 | .node₂ T1 T2 => T1.number_of_nodes + T2.number_of_nodes + 1
 
 
+-- ################################# --
 -- 4. Full Binary Trees with n nodes --
+--################################## --
 
 inductive full_binary_tree_with_n_nodes : (n : ℕ) → Type
 | leaf : full_binary_tree_with_n_nodes 0
 | node: {m n : ℕ} → full_binary_tree_with_n_nodes m → full_binary_tree_with_n_nodes n → full_binary_tree_with_n_nodes (n + m + 1)
 
 
+-- ############################### --
 -- 5. Ballot Sequences of Length n --
+-- ############################### --
 
 inductive ballot_sequence : ℕ → ℕ → Type
 | empty : ballot_sequence 0 0
@@ -65,28 +69,20 @@ inductive ballot_sequence : ℕ → ℕ → Type
 
 
 
-
+--------- ############ ---------
 --------- Larger Tasks ---------
+--------- ############ ---------
 
 
+-- ################################################# --
+-- 4. Bijection Between list PlaneTree and PlaneTree --
+-- ################################################# --
 
--- 3. Bijection between full binary trees and the type Fin Cn --
-
--- kr tezka :/
-
--- def full_binary_tree_to_fin_Cn : full_binary_tree_with_n_nodes (n : ℕ) → (n : Fin (catalan_number n))
--- | .leaf => 0
--- | .node =>
-
-
-
--- 4. Bijection Between list PlaneTree and PlaneTree
-
--- ideja: definiraš 2 funkciji (def):
---    - List PlaneTree -> PlaneTree
---    - PlaneTree -> List PlaneTree
+-- Idea: define two functions:
+--    - f  : List PlaneTree -> PlaneTree
+--    - f' : PlaneTree -> List PlaneTree
 --
--- in pokažeš (theorem) da če uporabiš najprej eno in pol drugo, da dobiš nazaj isto
+-- and show that f(f'(x)) = x and f'(f(x)) = x
 
 def plane_tree_to_list_plane_tree : plane_tree → List plane_tree
 | .node N => N
@@ -105,10 +101,13 @@ theorem list_plane_tree_of_plane_tree_of_list_plane_tree_is_id :
 exact fun t ↦ rfl
 
 
-
+-- ################################## --
 -- 6. 2n choose n is divisible by n+1 --
+-- ################################## --
 
--- #eval Nat.choose 10 2
+
+-- We tried to follow this proof: https://math.stackexchange.com/a/189350
+-- Our idea was to first prove the equality - lemma "equality"
 
 lemma two_n_minus_n_eq_n (n : ℕ) :
 2 * n - n = n := by
@@ -118,19 +117,60 @@ lemma two_n_minus_n_plus_one_eq_n_minus_one (n : ℕ) :
 2 * n - (n + 1) = n - 1 := by
 omega
 
-lemma one_over_n_minus_one_factorial_eq_n_over_n_factorial (n : ℕ) (h : n > 0) :
-1 / Nat.factorial (n - 1) = n / Nat.factorial n := by
-apply Nat.div_eq_of_eq_mul_right
-apply Nat.factorial_pos
--- rw [← mul_div_assoc] -- zakaj to ne dela? Če uporabimo spodnjo, dobimo še pogoj n! | n ...
-rw [← Nat.mul_div_assoc]
-rw [mul_comm, Nat.mul_factorial_pred]
-rw [Nat.div_self]
-apply Nat.factorial_pos
-apply h
+lemma equality (n : ℕ) (h : n > 0):
+Nat.choose (2 * n) (n + 1) = n / (n + 1) * Nat.choose (2 * n) n := by
+  rw [Nat.choose_eq_factorial_div_factorial, Nat.choose_eq_factorial_div_factorial]
+  rw [two_n_minus_n_eq_n, two_n_minus_n_plus_one_eq_n_minus_one]
+  apply Nat.div_eq_of_eq_mul_right
+  apply Nat.mul_pos
+  apply Nat.factorial_pos
+  apply Nat.factorial_pos
+  rw [Nat.factorial_succ]
+  -- At this point the proof is obvious on paper.
+  -- We first wanted to cancel the n+1 one the right, as it appears once in the denominator and once normally.
+  -- The problem is that this seems impossible in lean - the division used is division with remainder, which makes
+  -- the lemma false and therefore non-provable.
+  sorry
+  omega
+  omega
+
+-- We may try to solve this problem by casting one of the factors to a rational number - for example n+1, which
+-- forces the division to operate using rationals. The lemma is therefore true, but it is questionable if it can
+-- be proved.
+lemma equality_q (n : ℕ) (h : n > 0):
+Nat.choose (2 * n) (n + 1) = n / ((n + 1) : ℚ) * Nat.choose (2 * n) n := by
+  rw [Nat.choose_eq_factorial_div_factorial, Nat.choose_eq_factorial_div_factorial]
+  rw [two_n_minus_n_eq_n, two_n_minus_n_plus_one_eq_n_minus_one]
+  sorry
+  omega
+  omega
+
+
+-- The rest of the proof is divided into cases n=0 and n>0, as we (may) need the h : n > 0 in the proof. For n=0
+-- the proof is trivial
+
+-- n > 0
+theorem n_plus_one_divides_2n_choose_n_non_zero (n : Nat) (h : n > 0) :
+n + 1 ∣ Nat.choose (2 * n) n := by
 sorry
 
--- tole je samo kot helper: če se nam v equality rata znebit imenovalcev
+-- The main theorem
+theorem n_plus_one_divides_2n_choose_n (n : Nat) :
+n + 1 ∣ Nat.choose (2 * n) n := by
+match n with
+  | 0 =>
+    omega
+  | Nat.succ k =>
+    apply n_plus_one_divides_2n_choose_n_non_zero (Nat.succ k)
+    exact Nat.zero_lt_succ k
+
+---------------------------------------------------
+-- some additional code that we tried for 6., but did not use in the end:
+
+-- how we figured out that the division is integer division with remainder:
+#eval (λ (m : ℕ) => (m / (m + 1))) 10
+
+-- if we managed to bring the "equality" lemma to a form with no denominators, this would complete the proof:
 lemma no_denoms (n : ℕ) (h : n > 0) :
 Nat.factorial (2 * n) * (n + 1) * Nat.factorial n * Nat.factorial n = n * Nat.factorial (2 * n) * Nat.factorial (n - 1) * Nat.factorial (n + 1) := by
 rw [mul_comm n (Nat.factorial (2 * n))]
@@ -142,49 +182,91 @@ rw [mul_comm (Nat.factorial n) (Nat.factorial (n + 1))]
 apply h
 
 
-variable (m : ℕ)
-#eval (λ (m : ℕ) => (m / (m - 1))) 10
+-- ####################### --
+-- 5. rotating isomorphism --
+-- ####################### --
+
+-- Idea: to prove the rotating isomorphism between full_binary_tree and plane_tree
+-- we defined two mutually inverse functions: plane_tree_of_full_binary_tree and full_binary_tree_of_plane_tree
+-- and then tried to show that composing these functions in either order results in the identity function
+
+-- transforms full_binary_tree to plain_tree
+def plane_tree_of_full_binary_tree : full_binary_tree → plane_tree
+| full_binary_tree.leaf => plane_tree.node [] -- If the full_binary_tree is a leaf, it transforms to a node with an empty list of children
+| full_binary_tree.node₂ T1 T2 => -- Transforms to a plane_tree.node containing a list with the transformed versions of T1 and T2
+    plane_tree.node [plane_tree_of_full_binary_tree T1, plane_tree_of_full_binary_tree T2]
+
+-- transforms a plane_tree to a full_binary_tree
+def full_binary_tree_of_plane_tree : plane_tree → full_binary_tree
+| plane_tree.node [] => full_binary_tree.leaf
+| plane_tree.node (left :: right :: rest) => -- A node with a list containing at least two elements in plane_tree transforms to a node₂ in full_binary_tree with the transformed left subtree and the rest of the list recursively transformed
+    full_binary_tree.node₂
+      (full_binary_tree_of_plane_tree left)
+      (full_binary_tree_of_plane_tree (plane_tree.node (right :: rest)))
+| plane_tree.node _ => full_binary_tree.leaf -- Exactly one child
+
+-- Theorem for converting a full_binary_tree.leaf to a plane_tree.node []
+theorem plane_tree_of_leaf : plane_tree_of_full_binary_tree full_binary_tree.leaf = plane_tree.node [] :=
+  rfl
+
+-- Theorem for converting a full_binary_tree.node₂ to a plane_tree.node
+-- converting a full_binary_tree.node₂ results in a plane_tree.node containing a list with the converted left and right subtrees
+theorem plane_tree_of_node₂ (left : full_binary_tree) (right : full_binary_tree) :
+  plane_tree_of_full_binary_tree (full_binary_tree.node₂ left right) =
+  plane_tree.node [plane_tree_of_full_binary_tree left, plane_tree_of_full_binary_tree right] :=
+  rfl
+
+-- Theorem for converting a plane_tree.node [] to a full_binary_tree.leaf
+theorem full_binary_tree_of_leaf : full_binary_tree_of_plane_tree (plane_tree.node []) = full_binary_tree.leaf :=
+  rfl
+
+-- Theorem for converting a plane_tree.node with a list containing left and right trees
+theorem full_binary_tree_of_cons (left : plane_tree) (rights : List plane_tree) :
+  full_binary_tree_of_plane_tree (plane_tree.node (left :: rights)) =
+    full_binary_tree.node₂
+      (full_binary_tree_of_plane_tree left)
+      (full_binary_tree_of_plane_tree (plane_tree.node rights)) :=
+  by
+    -- dsimp [full_binary_tree_of_plane_tree]
+    -- match rights with
+    -- | [] => rfl
+    -- | [right] => rfl
+    -- | right :: rest => rfl
+    sorry
 
 
-lemma equality (n : ℕ) (h : n > 0):
-Nat.choose (2 * n) (n + 1) = n / (n + 1) * Nat.choose (2 * n) n := by
-rw [Nat.choose_eq_factorial_div_factorial, Nat.choose_eq_factorial_div_factorial]
-rw [two_n_minus_n_eq_n, two_n_minus_n_plus_one_eq_n_minus_one]
-apply Nat.div_eq_of_eq_mul_right
-apply Nat.mul_pos
-apply Nat.factorial_pos
-apply Nat.factorial_pos
-rw [Nat.factorial_succ]
-rw [← mul_assoc]
-nth_rw 2 [mul_assoc]
-rw [mul_comm (Nat.factorial (n - 1)) (n / (n + 1))]
-rw [← mul_assoc]
-rw [mul_assoc (n + 1) (Nat.factorial n) (n / (n + 1))]
-rw [mul_comm (Nat.factorial n) (n / (n + 1))]
-rw [← mul_assoc]
-rw [mul_assoc, mul_assoc]
-sorry -- res nm je žou
-omega
-omega
-
-
-lemma a (n : ℕ) :
-2 * n = (1 + 1) * n := by
-apply Nat.eq_of_mul_eq_mul_right
-
-
-
--- rešimo za n>0
-theorem n_plus_one_divides_2n_choose_n_non_zero (n : Nat) (h : n > 0) :
-n + 1 ∣ Nat.choose (2 * n) n := by sorry
-
-
--- razdelimo na 0 in succ: 0 je obv, n>0 pa rabimo kot predpostavko za naprej
-theorem n_plus_one_divides_2n_choose_n (n : Nat) :
-n + 1 ∣ Nat.choose (2 * n) n := by
-match n with
-  | 0 =>
-    omega
-  | Nat.succ k =>
-    apply n_plus_one_divides_2n_choose_n_non_zero (Nat.succ k)
-    exact Nat.zero_lt_succ k
+-- we want to show that composing functions plane_tree_of_full_binary_tree and full_binary_tree_of_plane_tree in either order results in the identity function
+def rotating_iso : full_binary_tree ≃ plane_tree :=
+{ toFun := plane_tree_of_full_binary_tree,
+  invFun := full_binary_tree_of_plane_tree,
+  left_inv := by
+    intro f -- full_binary_tree_of_plane_tree (plane_tree_of_full_binary_tree f) = f
+    induction f with
+    | leaf => exact full_binary_tree_of_leaf
+    | node₂ T1 T2 IH_T1 IH_T2 =>
+      rw [plane_tree_of_node₂, full_binary_tree_of_cons]
+      congr
+      -- exact IH_T1
+      -- exact IH_T2,
+      sorry
+  right_inv := by
+    intro p -- plane_tree_of_full_binary_tree (full_binary_tree_of_plane_tree p) = p
+    induction p with -- we got stuck with this issue but tried to prove it theoretically
+    | node branches =>
+      cases branches with
+      | nil => rw [full_binary_tree_of_leaf, plane_tree_of_full_binary_tree]
+      | cons left rights =>
+        cases rights with
+        | nil => rw [full_binary_tree_of_plane_tree, plane_tree_of_full_binary_tree]
+        | cons right rights =>
+          cases rights with
+          | nil =>
+            rw [full_binary_tree_of_plane_tree, plane_tree_of_full_binary_tree, full_binary_tree_of_cons]
+            simp
+          | cons _ _ =>
+            have h : full_binary_tree_of_plane_tree (plane_tree.node (left :: right :: rights)) =
+              full_binary_tree.node₂ (full_binary_tree_of_plane_tree left)
+              (full_binary_tree_of_plane_tree (plane_tree.node (right :: rights))) :=
+                by rw [full_binary_tree_of_cons]
+            rw [h]
+            simp }
